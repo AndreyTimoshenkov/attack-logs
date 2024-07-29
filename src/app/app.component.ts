3e78cc310c4d06a92aa1665ee45c1476cc1af4f6
Andrey Timoshenkov <andreytmshnkv@gmail.com>
2024-07-29 11:18:55 +0300

    init app, create data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './data.service';
+import { CommonModule } from '@angular/common';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, HttpClientModule, CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
+  providers: [DataService]
 })
 export class AppComponent {
-  title = 'attack-logs';
+
+  private readonly _initUrl: string = 'assets/files/attack_logs.json';
+  private readonly _updateUrl: string = 'assets/files/update_logs.json';
+
+  readonly data$ = this.dataService.getData(this._initUrl).pipe(
+    map(json => JSON.parse(JSON.stringify(json)).logs)
+  )
+  // .subscribe(data => console.log(data.logs));
+
+  constructor(private dataService: DataService) {}
 }
